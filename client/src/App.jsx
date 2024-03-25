491e1d6c6fdada11c789d77077a049fd36fa0c5b
b-price <bprice12@students.towson.edu>
2024-03-25 17:16:47 -0400

    Initial commit, sample vite/react code

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Sample from './components/Sample'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Sample />
+    </>
+  )
+}
+
+export default App
